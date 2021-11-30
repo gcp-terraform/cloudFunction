8aa1d69cef88f92206384bf04a63715fbefc8608
marly10 <mckmarly@icloud.com>
2021-11-30 12:11:13 -0800

    creating test gke new ADDEhhttDg

--- gke.tf
+++ gke.tf
@@ -13,6 +13,24 @@ variable "gke_num_nodes" {
   description = "number of gke nodes"
 }
 
+resource "google_compute_network" "vpc" {
+  project                 = var.project_id
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
