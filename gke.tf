59ba11f803e7dc1f8a45d33151d27c26cc77120a
marly10 <mckmarly@icloud.com>
2021-11-30 16:22:23 -0800

    creating test gke new ADDEhhttDlgsswerws;"

--- gke.tf
+++ gke.tf
@@ -40,13 +40,13 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
+  //remove_default_node_pool = true
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 }
-
+/*
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
@@ -73,7 +73,7 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
-
+*/
 # # Kubernetes provider
 # # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
 # # It references the variables and resources provisioned in this file. 
