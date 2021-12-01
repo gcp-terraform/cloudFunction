c4524b8211a116611e15b06aad7401ff97d6e77c
marly10 <mckmarly@icloud.com>
2021-12-01 10:44:41 -0800

    pub target

--- gke.tf
+++ gke.tf
@@ -46,13 +46,13 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   //remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count = 1
 
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
-  
+
 }
 
 
